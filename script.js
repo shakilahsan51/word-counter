851e46757506780733443afa75abdd3cbaf32d6d
shakilahsan51 <shakilahsan51@gmail.com>
2024-12-28 21:03:13 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+//   let textbox = document.getElementById("textbox");
+//   textbox.addEventListener("input", function () {
+//     text = this.value;
+//     let char = text.length;
+//     document.getElementById("char").innerHTML = char;
+
+//     text = text.trim();
+//     let words = text.split(" ");
+//     let cleanList = words.filter(function (elm) {
+//       return elm != "";
+//     });
+//     console.log(cleanList);
+//     document.getElementById("word").innerHTML = cleanList.length;
+//   });
